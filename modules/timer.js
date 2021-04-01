a4134d9584f8a39c3d4c4f1fa13a6ecc79fc1311
MianAnswer <miananswer@hotmail.com>
2021-03-31 23:34:31 -0400

    Update and clean up timer logic

--- modules/timer.js
+++ modules/timer.js
@@ -1,39 +1,81 @@
 export class Timer {
 
-    constructor(startTime, callback) {
-        this.startTime = startTime;
-        this.difference = 0;
-        this.time = 0;
+    /**
+     * constructs Timer
+     * @param {number} countdown (in milliseconds)
+     * @param {function} callback 
+     */
+    constructor(countdown, callback) {
+        this.isTimerRunning = false;
+        this.isCountdownFinished = false;
+        this.remainingCountdown = countdown;
+        this.originalCountdown = countdown;
         this.timer = 0;
         this.callback = callback;
     }
 
-    start() {
-        if (!this.timer) {
-            this.time = Date.now();
-            this.startTime -= this.difference;
-            this.timer = setInterval(()=> {
-                this.countdown();
-            }, 1000);
-        }
+    /**
+     * sets countdown
+     * @param {number} countdown 
+     */
+    setCountdown(countdown) {
+        this.originalCountdown = countdown;
     }
 
+    /**
+     * start timer
+     */
+    start() {
+        return new Promise(resolve => {
+            if (!this.isTimerRunning) {
+                // reset countdown to original when countdown is finished
+                if (this.isCountdownFinished) {
+                    this.remainingCountdown = this.originalCountdown;
+                }
+                this.isTimerRunning = true;
+                this.isCountdownFinished = false;
+
+                this.timer = setInterval(() => {
+                    this.countdown();
+                    if (this.isCountdownFinished) {
+                        resolve();
+                    }
+                }, 1000);
+            }
+        })
+    }
+
+    /**
+     * pause timer
+     */
     pause() {
-        if (this.timer) {
+        if (this.isTimerRunning) {
+            this.isTimerRunning = false;
             this.timer = clearInterval(this.timer);
         }
     }
 
+    /**
+     * counts down the time
+     */
     countdown() {
-        this.difference = Date.now() - this.time;
-        this.difference = Math.floor(this.difference / 1000);
-console.log(this.difference);
-        if (this.difference <= this.startTime) {
-            this.callback(this.startTime - this.difference);
+        if (this.remainingCountdown >= 0) {
+            console.log(this.remainingCountdown);
+            // do something with the current countdown
+            this.callback(this.remainingCountdown);
         }
         else {
+            // reset countdown
             console.log('Timer finished');
+            this.isTimerRunning = false;
+            this.isCountdownFinished = true;
+            this.remainingCountdown = this.originalCountdown;
+
+            // clear timer
             clearInterval(this.timer);
         }
+
+        // subtracts 1 second from the countdown
+        this.remainingCountdown -= 1000;
     }
 }
